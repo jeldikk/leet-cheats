2de7c9938a7c99620adcf2c83553f6387a726d6d
jeldikk <jeldi.kamal2013@gmail.com>
2024-12-10 19:22:32 +0530

    add basic test suites for Queue

---
+++ javascript/src/tests/queue.test.js
@@ -0,0 +1,7 @@
+describe("Queue Test Suite", () => {
+  describe("Empty Queue Test suite", () => {});
+
+  describe("Unlimited Queue Test Suite", () => {});
+
+  describe("LImited Queue Test Suite", () => {});
+});
